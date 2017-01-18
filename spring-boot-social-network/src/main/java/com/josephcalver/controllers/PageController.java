32d090a86915c0dfa8f7231c2cebd065395564bf
CaolanandJo <CaolanandJo@192.168.1.2>
2017-01-18 17:11:53 +0000

    Initial setup and tiles configuration

---
+++ spring-boot-social-network/src/main/java/com/josephcalver/controllers/PageController.java
@@ -0,0 +1,24 @@
+package com.josephcalver.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping("/")
+	String home() {
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	String about() {
+		return "about";
+	}
+
+	@RequestMapping("/addstatus")
+	String addStatus() {
+		return "addstatus";
+	}
+
+}
---
+++ spring-boot-social-network/src/main/webapp/WEB-INF/tiles/home.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<h1 class="display-4">Hello, world — tiles!</h1>
